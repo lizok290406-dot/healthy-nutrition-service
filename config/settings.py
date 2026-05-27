edfb176ffe10ab0f9ab67c71d147ad26ffb881a5
Лиза <lizok290406@gmail.com>
2026-05-27 18:11:23 +0500

    Update allowed hosts for deployment

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^1ru#&16+29n#e#5$iz#8l##k5&1=2cz)=&#28ux=cp3l)fs91
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'lizok290406-dot.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'pollizko.pythonanywhere.com']
 
 
 # Application definition
